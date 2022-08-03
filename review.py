403e3a98bd553c024e3468e80510d84364338753
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-08-03 21:33:44 +0200

    write to clang_tidy_review.yaml file

--- review.py
+++ review.py
@@ -1,5 +1,41 @@
 #!/usr/bin/env python3
 
+# clang-tidy review
+# Copyright (c) 2020 Peter Hill
+# SPDX-License-Identifier: MIT
+# See LICENSE for more information
+
+import argparse
+import contextlib
+import datetime
+import itertools
+import fnmatch
+import json
+import os
+from operator import itemgetter
+import pathlib
+import pprint
+import re
+import requests
+import subprocess
+import textwrap
+import unidiff
+import yaml
+from github import Github
+from github.Requester import Requester
+from github.PaginatedList import PaginatedList
+from typing import List
+
+
 if __name__ == "__main__":
-    print("review hello")
-    print('::set-output name=test::{"foo":"bar"}')
+    parser = argparse.ArgumentParser(
+        description="Create a review from clang-tidy warnings"
+    )
+    parser.add_argument("--token", help="github auth token")
+
+    args = parser.parse_args()
+
+    with open('clang_tidy_review.yaml', 'w') as f:
+        f.write('foobar')
+
+    github = Github(args.token)
