79e1d90550c799907705e1244e45064cc5ecb0c0
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-08-03 22:15:23 +0200

    aaaaa

--- post_comments/post_comments.py
+++ post_comments/post_comments.py
@@ -2,6 +2,6 @@
 
 if __name__ == "__main__":
     print("post_comments hello")
-    with open('clang-tidy-review/clang-tidy-review.yaml', 'r') as f:
+    with open('clang-tidy-review.yaml', 'r') as f:
         print(f.read())
     print("post_comments bye")
